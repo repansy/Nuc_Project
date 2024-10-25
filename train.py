4d3f128deb9065256526a18fee53875b149d46d3
repansy <3033372070@qq.com>
2024-10-25 22:21:02 +0800

    change train to use swinIR(no test)

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ from torchvision import transforms
 import torchvision.utils as utils
 # from torchvision.models import ResNet
 from models.DnCNN import DnCNN
-# from models import SwinIR
+from models.SwinIR import SwinIR
 from datasets.dataset import pre_h5_dataset
 from utils.ValFunc import weights_init_kaiming, batch_PSNR
 from pretreatment import  prepare_data
@@ -80,7 +80,7 @@ def DCNN_train():
         for i, data in enumerate(loader_train, 0):
             # training step
             model.train()
-            model.zero_grad() # 存疑
+            model.zero_grad()
             optimizer.zero_grad()
             img_train = data
             
@@ -133,12 +133,98 @@ def DCNN_train():
         # save model
         torch.save(model.state_dict(), os.path.join(opt.outf, 'net.pth'))
 
+def SWIR_train():
+    # Load dataset
+    print('Loading dataset ...\n')
+    dataset_train = pre_h5_dataset(train=True)
+    dataset_val = pre_h5_dataset(train=False)
+    loader_train = DataLoader(dataset=dataset_train, num_workers=4, batch_size=opt.batchSize, shuffle=True)
+    # loader_value = DataLoader(dataset=dataset_val, num_workers=4, batch_size=opt.batchSize, shuffle=True)
+    print("# of training samples: %d\n" % int(len(dataset_train)))
+    # Build model
+    net = SwinIR(img_size=40, patch_size=1, in_chans=1, window_size=8)
+    criterion = nn.MSELoss(size_average=False)
+    # Move to GPU
+    device_ids = [0]
+    model = nn.DataParallel(net, device_ids=device_ids).cuda()
+    criterion.cuda()
+    criterion = nn.MSELoss(size_average=False)
+    # Optimizer
+    optimizer = optim.Adam(model.parameters(), lr=opt.lr)
 
+    # training
+    writer = SummaryWriter(opt.outf)
+    step = 0
+    for epoch in range(opt.epochs):
+        if epoch < opt.milestone:
+            current_lr = opt.lr
+        else:
+            current_lr = opt.lr / 10.
+        # set learning rate
+        for param_group in optimizer.param_groups:
+            param_group["lr"] = current_lr
+        print('learning rate %f' % current_lr)
+        # train
+        for i, data in enumerate(loader_train, 0):
+            # training step
+            model.train()    # model.zero_grad() 目前不需要
+            optimizer.zero_grad()
+            img_train = data
+            
+            # noise 添加
+            noise = NoiseGenerator(img_train, 25).stripe_noise(True, 'col', 0.85)
+            imgn_train = img_train + noise
+            img_train, imgn_train = Variable(img_train.cuda()), Variable(imgn_train.cuda())
+            noise = Variable(noise.cuda()) # noise = noise.cuda() # 
+            out_train = model(imgn_train)
+            loss = criterion(out_train, noise) / (imgn_train.size()[0]*2)
+            loss.backward()
+            optimizer.step()
+            # results
+            model.eval()
+            out_train = torch.clamp(imgn_train-model(imgn_train), 0., 1.)
+            psnr_train = batch_PSNR(out_train, img_train, 1.)
+            print("[epoch %d][%d/%d] loss: %.4f PSNR_train: %.4f" %
+                 (epoch+1, i+1, len(loader_train), loss.item(), psnr_train))
+            # if you are using older version of PyTorch, you may need to change loss.item() to loss.data[0]
+            if step % 10 == 0:
+                # Log the scalar values
+                writer.add_scalar('loss', loss.item(), step)
+                writer.add_scalar('PSNR on training data', psnr_train, step)
+            step += 1
+        #  the end of each epoch
+        model.eval()
+        # validate
+        psnr_val = 0
+        for data in dataset_val:
+            img_val = data
+            # noise = stripe_noise(img_val, 0, 30, 10)
+            noise = NoiseGenerator(img_train, 25).stripe_noise(True, 'col', 0.85)
+            imgn_val = img_val + noise
+            img_val = torch.unsqueeze(img_val, 0)
+            img_val, imgn_val = Variable(img_val.cuda(), volatile=True), Variable(imgn_val.cuda(), volatile=True)
+            out_val = torch.clamp(imgn_val - model(imgn_val), 0., 1.)
+            psnr_val += batch_PSNR(out_val, img_val, 1.)
+        psnr_val /= len(dataset_val)
+        print("\n[epoch %d] PSNR_val: %.4f" % (epoch + 1, psnr_val))
+        writer.add_scalar('PSNR on validation data', psnr_val, epoch)
+
+        # log the images
+        out_train = torch.clamp(imgn_train-model(imgn_train), 0., 1.)
+        Img = utils.make_grid(img_train.data, nrow=8, normalize=True, scale_each=True)
+        Imgn = utils.make_grid(imgn_train.data, nrow=8, normalize=True, scale_each=True)
+        Irecon = utils.make_grid(out_train.data, nrow=8, normalize=True, scale_each=True)
+        writer.add_image('clean image', Img, epoch)
+        writer.add_image('noisy image', Imgn, epoch)
+        writer.add_image('reconstructed image', Irecon, epoch)
+        # save model
+        torch.save(model.state_dict(), os.path.join(opt.outf, 'net.pth'))
+    
 if __name__ == '__main__':
-    data_path = r'data\\h5_data'
+    data_path = r"./Nuc_Project/data"
     if opt.preprocess:
         if opt.mode == 'S':
             prepare_data(data_path=data_path, patch_size=40, stride=10, aug_times=1)
         if opt.mode == 'B':
             prepare_data(data_path=data_path, patch_size=50, stride=10, aug_times=2)
-    DCNN_train()
+    SWIR_train()
